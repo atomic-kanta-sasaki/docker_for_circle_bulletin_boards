1c4adaaefd13f36fda3ca88dd6175485dac7aa64
kanta_sasaki <k_padbjw@icloud.com>
2020-09-05 16:34:10 +0900

    create circle detail table

--- docker/mysql/initdb.d/schema.sql
+++ docker/mysql/initdb.d/schema.sql
@@ -4,3 +4,13 @@ CREATE TABLE users (
     email VARCHAR(32) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE circle_details (
+    circle_id INT NOT NULL,
+    university VARCHAR(32) NOT NULL,
+    active_introduction VARCHAR(32) NOT NULL,
+    subscription_condition VARCHAR(32) NOT NULL,
+    twitter VARCHAR(32),
+    instagram VARCHAR(32),
+    facebook VARCHAR(32)
+)
