fe8e98df13fc7766cc5c9f65df8448238b308348
kanta_sasaki <k_padbjw@icloud.com>
2020-09-08 19:39:00 +0900

    repair encoding

--- docker/mysql/initdb.d/schema.sql
+++ docker/mysql/initdb.d/schema.sql
@@ -1,17 +1,21 @@
+SET CHARSET UTF8;
+CREATE DATABASE IF NOT EXISTS sample_database DEFAULT CHARACTER SET utf8;
+
+
 CREATE TABLE circles (
     id INT NOT NULL AUTO_INCREMENT,
-    university TEXT NOT NULL,
+    university varchar(100) NOT NULL,
     circle_name VARCHAR(32) NOT NULL,
     email VARCHAR(32) NOT NULL,
     PRIMARY KEY (id)
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='通知タイプテーブル';
 
 CREATE TABLE circle_details (
     circle_id INT NOT NULL,
     active_introduction TEXT NOT NULL,
     subscription_condition TEXT NOT NULL,
-    twitter TEXT,
-    instagram TEXT,
-    facebook TEXT,
-    thumbnail TEXT
-);
+    twitter varchar(100),
+    instagram varchar(100),
+    facebook varchar(100),
+    thumbnail varchar(100)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='通知タイプテーブル';
